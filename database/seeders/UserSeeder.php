0a104acbd91c223f2ef0fe4e8521e75c17172e8a
indromhars <indra.madaharsa@gmail.com>
2024-02-24 18:51:44 +0700

    change the route name for users management page

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use  Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,5 +16,12 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         //
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('users')->insert([
+                'name'      => Str::random(10),
+                'email'     => Str::random(15).@'example.com',
+                'password'  => Hash::make('pasword'),
+            ]);
+        }
     }
 }
